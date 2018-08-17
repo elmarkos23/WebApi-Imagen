34a3c31e513c5a1d16e01a40b6dacfa2e61d3a60
elmarkos23 <elmarkos23@gmail.com>
2018-08-17 15:19:19 -0500

    Terminado

---
+++ Controllers/ImagenController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApi_Imagen.Controllers
+{
+    public class ImagenController : ApiController
+    {
+        [Route("api/Imagen/Upload")]
+        public async Task<string> Post()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+
+                if (httpRequest.Files.Count > 0)
+                {
+                    foreach (string file in httpRequest.Files)
+                    {
+                        var postedFile = httpRequest.Files[file];
+                        var fileName = postedFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+                        var filePath = HttpContext.Current.Server.MapPath("~/Images/" + fileName);
+                        postedFile.SaveAs(filePath);
+                        return "/Images/" + fileName;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                return exception.Message;
+            }
+
+            return "no files";
+        }
+    }
+}
